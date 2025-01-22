d8a3b7ad4c9fc1f92bd7be262ceab152c308497d
YunosukeA <aoforuniversity@gmail.com>
2025-01-22 20:00:49 +0900

    wip

--- src/ObserverbasedAdmittance.cpp
+++ src/ObserverbasedAdmittance.cpp
@@ -1,23 +1,64 @@
 #include "ObserverbasedAdmittance.h"
 
-ObserverbasedAdmittance::ObserverbasedAdmittance(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config)
-: mc_control::MCController(rm, dt)
-{
+ObserverbasedAdmittance::ObserverbasedAdmittance(
+    mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration &config)
+    : mc_control::MCController(rm, dt) {
+
   solver().addConstraintSet(contactConstraint);
   solver().addConstraintSet(kinematicsConstraint);
   solver().addTask(postureTask);
-  solver().setContacts({{}});
 
   mc_rtc::log::success("ObserverbasedAdmittance init done ");
 }
 
-bool ObserverbasedAdmittance::run()
-{
-  return mc_control::MCController::run();
+void ObserverbasedAdmittance::configure(const mc_control::MCController &ctl,
+                                        const mc_rtc::Configuration &config) {
+  MaxContacts = config("MaxContacts", 4);
+
+  robot_ = config("robot", ctl.robot().name());
+  if (config.has("exportValue")) {
+    auto exportValueConfig = config("exportValue");
+    if (exportValueConfig.has("exportContactWrench")) {
+      exportValueConfig("exportContactWrench", exportContactWrench_);
+    }
+    if (exportValueConfig.has("exportExternalWrench")) {
+      exportValueConfig("exportExternaltWrench", exportExternalWrench_);
+    }
+  }
 }
 
-void ObserverbasedAdmittance::reset(const mc_control::ControllerResetData & reset_data)
-{
+bool ObserverbasedAdmittance::run(mc_control::MCController &ctl) {
+
+  if (exportContactWrench_) {
+    for (int i = 0; i < MaxContacts; i++) {
+      //   if (ctl.datastore().has(
+      //           robot_ + "::estimatedContactWrench_" +
+      //           std::to_string(observer_.contactIndexNormal(i)))) {
+      //   auto &getEstimatedConWrench =
+      //     ctl.datastore().get<Eigen::Vector6d>(robot_ +
+      //     "::estimatedContactWrench_" +
+      //                         std::to_string(observer_.contactIndexNormal(i));
+
+      //     mc_rtc::log::info("Estimated Wrench {}: {}", i,
+      //     getEstimatedConWrench());
+      //   }
+      // };
+    }
+    if (exportExternalWrench_) {
+      if (ctl.datastore().has(robot_ + "::estimatedExternalWrench")) {
+        auto &getEstimatedExtWrench = ctl.datastore().get<Eigen::Vector6d>(
+            robot_ + "::estimatedExternalWrench");
+
+        mc_rtc::log::info("Estimated External Wrench: {}",
+                          getEstimatedExtWrench.transpose());
+      }
+      return mc_control::MCController::run();
+    }
+  }
+}
+
+void ObserverbasedAdmittance::reset(
+    const mc_control::ControllerResetData &reset_data) {
   mc_control::MCController::reset(reset_data);
 }
 
--- src/ObserverbasedAdmittance.h
+++ src/ObserverbasedAdmittance.h
@@ -1,15 +1,28 @@
 #pragma once
 
+#include "mc_state_observation/MCKineticsObserver.h"
+#include <mc_control/MCController.h>
 #include <mc_control/mc_controller.h>
-
 
 #include "api.h"
 
-struct ObserverbasedAdmittance_DLLAPI ObserverbasedAdmittance : public mc_control::MCController
-{
-  ObserverbasedAdmittance(mc_rbdyn::RobotModulePtr rm, double dt, const mc_rtc::Configuration & config);
+struct ObserverbasedAdmittance_DLLAPI ObserverbasedAdmittance
+    : public mc_control::MCController {
+  ObserverbasedAdmittance(mc_rbdyn::RobotModulePtr rm, double dt,
+                          const mc_rtc::Configuration &config);
 
-  bool run() override;
+  bool run(mc_control::MCController &ctl);
 
-  void reset(const mc_control::ControllerResetData & reset_data) override;
+  void configure(const mc_control::MCController &ctl,
+                 const mc_rtc::Configuration &config);
+
+  void reset(const mc_control::ControllerResetData &reset_data) override;
+
+  // std::string &usingObserver_;
+  int MaxContacts;
+  std::string robot_;
+  // stateObservation::KineticsObserver observer_;
+
+  bool exportContactWrench_;
+  bool exportExternalWrench_;
 };
